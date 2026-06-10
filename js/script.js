ce77094ad4db97e68cac33a59ee77da7b0232421
Andrey Sidorkin <gholos@yandex.ru>
2026-06-10 18:14:39 +0700

    Create getComputerChoice func

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-console.log('Hello, World!');
+function getComputerChoice() {
+    let choices = ['Rock', 'Paper', 'Scissors'];
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
+console.log(getComputerChoice());
