8a08259cff475970898e372bec26ecbead34a5e8
Andrey Sidorkin <gholos@yandex.ru>
2026-06-11 13:28:24 +0700

    Add script for button to play game(console)

--- js/script.js
+++ js/script.js
@@ -1,61 +1,58 @@
+const buttons = document.querySelectorAll("button");
+let humanScore = 0;
+let computerScore = 0;
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    let humanChoice = e.target.getAttribute('data-btn');
+    playRound(humanChoice);
+  });
+});
+
 function getComputerChoice() {
   let choices = ["rock", "paper", "scissors"];
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
-function getHumanChoice() {
-  let humanChoice = prompt("Enter value: ", "");
-  return humanChoice.toLowerCase();
-}
+function playRound(humanChoice) {
+  const computerChoice = getComputerChoice();
 
-function playGame() {
-  let humanScore = 0;
-  let computerScore = 0;
-
-  function playRound() {
-    const humanChoice = getHumanChoice();
-    const computerChoice = getComputerChoice();
-
-    if (humanChoice === "rock") {
-      if (computerChoice === "paper") {
-        humanScore++;
-        console.log(`You win! ${humanChoice} beat ${computerChoice}`);
-      } else if (computerChoice === "scissors") {
-        computerScore++;
-        console.log(`You lose! ${computerChoice} beat ${humanChoice}`);
-      }
-      if (computerChoice === "rock") {
-        console.log("Tie!");
-      }
+  if (humanChoice === "rock") {
+    if (computerChoice === "paper") {
+      humanScore++;
+      console.log(`You win! ${humanChoice} beat ${computerChoice}`);
+    } else if (computerChoice === "scissors") {
+      computerScore++;
+      console.log(`You lose! ${computerChoice} beat ${humanChoice}`);
     }
-
-    if (humanChoice === "paper") {
-      if (computerChoice === "rock") {
-        humanScore++;
-        console.log(`You win! ${humanChoice} beat ${computerChoice}`);
-      } else if (computerChoice === "scissors") {
-        computerScore++;
-        console.log(`You lose! ${computerChoice} beat ${humanChoice}`);
-      }
-      if (computerChoice === "paper") {
-        console.log("Tie!");
-      }
-    }
-
-    if (humanChoice === "scissors") {
-      if (computerChoice === "paper") {
-        humanScore++;
-        console.log(`You win! ${humanChoice} beat ${computerChoice}`);
-      } else if (computerChoice === "rock") {
-        computerScore++;
-        console.log(`You lose! ${computerChoice} beat ${humanChoice}`);
-      }
-      if (computerChoice === "scissors") {
-        console.log("Tie!");
-      }
+    if (computerChoice === "rock") {
+      console.log("Tie!");
     }
   }
 
-}
+  if (humanChoice === "paper") {
+    if (computerChoice === "rock") {
+      humanScore++;
+      console.log(`You win! ${humanChoice} beat ${computerChoice}`);
+    } else if (computerChoice === "scissors") {
+      computerScore++;
+      console.log(`You lose! ${computerChoice} beat ${humanChoice}`);
+    }
+    if (computerChoice === "paper") {
+      console.log("Tie!");
+    }
+  }
 
-playGame();
+  if (humanChoice === "scissors") {
+    if (computerChoice === "paper") {
+      humanScore++;
+      console.log(`You win! ${humanChoice} beat ${computerChoice}`);
+    } else if (computerChoice === "rock") {
+      computerScore++;
+      console.log(`You lose! ${computerChoice} beat ${humanChoice}`);
+    }
+    if (computerChoice === "scissors") {
+      console.log("Tie!");
+    }
+  }
+}
